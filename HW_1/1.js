d5e0e0e5678cf77c2a542a7d742d02887d51a991
aversh1206 <55345427+aversh1206@users.noreply.github.com>
2020-04-05 09:03:23 +0300

    Add files via upload

---
+++ HW_1/1.js
@@ -0,0 +1,10 @@
+let name = "Генератор защитного поля";
+let price = "1000 кредитов";
+
+let result = `Выбран «Генератор защитного поля», цена за штуку 1000 кредитов`;
+console.log('result :', result);
+
+price = "2000 кредитов";
+
+result = `Выбран ${name}, цена за штуку ${price}`;
+console.log('result :', result);
